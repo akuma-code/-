5172d862ded7c129ec0e6f8486ec19a17a762ec0
akuma-code <akumapl86@gmail.com>
2021-03-12 01:45:00 +0300

    output Done!

--- Testing/test.js
+++ Testing/test.js
@@ -1,4 +1,4 @@
-async function gv() {
+async function gv() { //принмает размеры выдает стекла
     let h = await document.getElementById("h").value;
     let w = await document.getElementById("w").value;
     let l = await document.getElementById("levo").value;
@@ -8,136 +8,108 @@ async function gv() {
     let mstv = await document.getElementById("s2").style.opacity;
     let rstv = await document.getElementById("s3").style.opacity;
 
-    async function consumeValues() {
-        let left, right, mid, height;
-        let dr, drs, di, dsi;
 
-        if (system == "Proline") {
-            dr = 48;
-            di = 26.5;
-            dsi = 74.5;
-            drs = 96;
-        };
-        if (system == "Softline") {
-            dr = 51;
-            di = 25.5;
-            dsi = 76.5;
-            drs = 102;
-        };
+    let delta = [];
 
-        if (system == 'Euroline') {
-            dr = 54;
-            di = 28;
-            dsi = 84.5;
-            drs = 110.5;
-        };
+    let left, right, mid, hl, hr, hm;
+    let dr, di, dsi, drs; //дельта рама, шмпост, импост-створка, импост-рама
 
-        if (system == 'Premium82') {
-            dr = 58;
-            di = 32;
-            dsi = 78;
-            drs = 104;
-        };
+    if (system == "Proline") delta = [48, 26.5, 74.5, 96];
 
-        if (system == 'BasePlus') {
-            dr = 50;
-            di = 27;
-            dsi = 76;
-            drs = 99;
-        };
+    if (system == "Softline") delta = [51, 25.5, 76.5, 102];
 
-        if (system == 'Lux') {
-            dr = 55; //дельта рама
-            di = 27; //дельта импост
-            dsi = 79; //дельта импост-створка
-            drs = 109; // дельта рама-створка
-        };
-        if (system == 'Optima') {
-            dr = 55; //дельта рама
-            di = 24.5; //дельта импост
-            dsi = 79; //дельта импост-створка
-            drs = 109; // дельта рама-створка
-        };
-        if (system == 'Termoplus') {
-            dr = 55; //дельта рама
-            di = 27; //дельта импост
-            dsi = 76; //дельта импост-створка
-            drs = 104; // дельта рама-створка
+    if (system == 'Euroline') delta = [54, 28, 84.5, 110.5];
 
-        };
-        if (system == 'WHS72') {
-            dr = 45; //дельта рама
-            di = 24.5; //дельта импост
-            dsi = 74.5; //дельта импост-створка
-            drs = 95; // дельта рама-створка
-        };
+    if (system == 'Premium82') delta = [58, 32, 78, 104];
 
-        if (system == 'WHS60') {
-            dr = 44; //дельта рама
-            di = 23.5; //дельта импост
-            dsi = 71.5; //дельта импост-створка
-            drs = 92; // дельта рама-створка
-        };
+    if (system == 'BasePlus') delta = [50, 27, 76, 99];
+
+    if (system == 'Lux') delta = [55, 27, 79, 109];
+
+    if (system == 'Optima') delta = [55, 24.5, 79, 109];
+
+    if (system == 'Termoplus') delta = [55, 27, 76, 104];
+
+    if (system == 'WHS72') delta = [45, 25.5, 74.5, 95];
+
+    if (system == 'WHS60') delta = [44, 23.5, 71.5, 92];
+
+    [dr, di, dsi, drs] = delta;
 
 
-        if (l && r) {
-            //3 stvorki
-            left = (!lstv) ? l - dr - di : l - drs + dsi;
-            right = (!rstv) ? r - dr - di : r - drs - dsi;
-            mid = (!mstv) ? w - di * 2 : w - dsi * 2;
-            return [
-                [height.l, left],
-                [height.m, mid],
-                [height.r, right],
-            ]
-        };
-        if (!r) {
-            // 2 stvorki
-            left = (!lstv) ? l - dr - di : l - drs + dsi;
-            height.l = (!lstv) ? h - 2 * dr : h - 2 * drs;
-            right = (!mstv) ? w - l - dr - di : w - l - dsi - drs;
-            height.r = (!mstv) ? h - 2 * dr : h - 2 * drs;
-            mid = 0;
-            return [
-                [height.l, left],
-                [height.r, right]
-            ]
-        };
+    if (rama == "x1") {
+        //1 stvorka
+        document.getElementById("outside").innerHTML += `<p1><strong>single, system: ${system}</strong></p1><br>`;
 
-        if (!l && !r) {
-            //1 stvorka
-            left = (!lstv) ? w - 2 * dr : w - 2 * drs;
-            right = 0;
-            mid = 0;
-            height = (!lstv) ? h - 2 * dr : h - 2 * drs;
-            return [height, left]
-        };
+        console.log("single");
+        left = (!lstv) ? w - 2 * dr : w - 2 * drs;
+        right = 0;
+        mid = 0;
+        hl = (!lstv) ? h - 2 * dr : h - 2 * drs;
+        return [
+            [hl, left]
+        ]
+    };
+    if (rama == "x2") {
+        // 2 stvorki
+        document.getElementById("outside").innerHTML += `<p1><strong>double, system: ${system}</strong></p1><br>`;
+
+        console.log("double");
+        left = (!lstv) ? Math.floor(l - dr - di) : Math.floor(l - drs + dsi);
+        hl = (!lstv) ? h - 2 * dr : h - 2 * drs;
+        right = (!mstv) ? Math.floor(w - l - dr - di) : Math.floor(w - l - dsi - drs);
+        hr = (!mstv) ? h - 2 * dr : h - 2 * drs;
+        mid = 0;
+        return [
+            [hl, left],
+            [hr, right]
+        ]
+    };
+
+    if (rama == "x3") {
+        //3 stvorki
+        document.getElementById("outside").innerHTML += `<p1><strong>triple, system: ${system}</strong></p1><br>`;
+
+
+        console.log("triple");
+        left = (!lstv) ? Math.floor(l - dr - di) : Math.floor(l - drs - dsi);
+        hl = (!lstv) ? Math.floor(h - 2 * dr) : Math.floor(h - 2 * drs);
+        right = (!rstv) ? Math.floor(r - dr - di) : Math.floor(r - drs - dsi);
+        hr = (!rstv) ? Math.floor(h - 2 * dr) : Math.floor(h - 2 * drs);
+        mid = (!mstv) ? w - l - r - di * 2 : w - l - r - dsi * 2;
+        hm = (!mstv) ? h - 2 * dr : h - 2 * drs;
+        return [
+            [hl, left],
+            [hr, right],
+            [hm, mid],
+        ]
+    };
+
+}
+
+async function configout(text) {
+    let out = document.getElementById("outside");
+    let elem = await document.createElement("div");
+    let output = "";
+    let c = 1;
+    elem.className = "config"
+
+    for (let line of text) {
+        let s = await document.getElementById('s' + c).style.opacity;
+        console.log(line);
+
+        output += `<p1><b>`;
+        output += (s == 1) ? `Створка #${c}: ` : `Фикса #${c}: `;
+        output += `  </b>H = ${line[0]}mm, W = ${line[1]}mm<br></p1>`;
+        c++;
     }
+    elem.innerHTML = output;
+    out.append(elem)
 
 }
 
-function go() {
+function calc() {
     gv().then(
-        result => consumeValues()
+        result => configout(result)
     )
-}
-// left = (lstv == 0) ? l - dr - di : l - drs - dsi;
-// right = (rstv == 0) ? r - dr - di : r - drs - dsi;
-// mid = (mstv == 0) ? mid - di * 2 : mid - dsi * 2;
-
-
-
-// return [
-//     [system],
-//     [h, left, (lstv)],
-//     [h, mid, (mstv)],
-//     [h, right, (rstv)]
-// ]
-
-
-
-
-function out() {
-    gv().then(
-        result => consumeValues())
 }
