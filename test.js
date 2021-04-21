c79d3fbb9be9ca5df1447a2c09d1344d6048b9a6
akuma-code <akumapl86@gmail.com>
2021-04-22 00:10:04 +0300

    zh:ready TODO: zhbb

--- test.js
+++ test.js
@@ -18,35 +18,37 @@ class GLS {
 
 
     applyDelta(delta, gls = this.arr) {
-            if (gls.length !== delta.length) console.log(`разная длинна массивов ${gls.length} : ${delta.length}`);
-            let out = [];
-            out.length = 0;
-            for (let i = 0; i < gls.length; i++) {
-                out.push([gls[i] - delta[i]])
-            };
-            return out.join(",").split(",")
-        }
-        //!----------<<<-----------  это в отдельную функцию вынести------------->>>-------------//
-
-    applyZs(glasses = this.arr) {
-        let dep = document.getElementById('gdepth').value;
-        let sys = document.getElementById('prof').value;
-        let zw, zh;
-        let dz = (ztype == "Rollite") ? SizeDB[sys]["rd" + dep] : SizeDB[sys].idpt;
-        [zw, zh] = dz;
+        if (gls.length !== delta.length) console.log(`разная длинна массивов ${gls.length} : ${delta.length}`);
         let out = [];
-        for (const glass of glasses) {
-            out.push(glass[0] - zw, glass[1] - zh)
-        }
-        return console.log(out);
+        out.length = 0;
+        for (let i = 0; i < gls.length; i++) {
+            out.push([gls[i] - delta[i]])
+        };
+        return out.join(",").split(",")
     }
+
+}
+
+function applyZs(glasses = []) {
+    let dep = document.getElementById('gdepth').value;
+    let sys = document.getElementById('prof').value;
+    let ztype = document.getElementById('ztype').innerText;
+    let zw, zh;
+    let dz = (ztype == "Rollite") ? SizeDB[sys]["rd" + dep] : SizeDB[sys].idpt;
+    [zw, zh] = dz;
+    let out = [];
+    for (const glass of glasses) {
+        out.push([glass[0] - zw, glass[1] - zh])
+    }
+    return out;
 }
 
 
 //! === Контейнер для выбора дельты стекла в зависимости от положения в раме
 let Delta_selector = {
-    //! === glass type 0 === [r-r] -> рама-рама
     sys() { return document.getElementById('prof').value },
+
+    //! === glass type 0 === [r-r] -> рама-рама
     rr(isfix) {
         if (isfix > 1 || isfix < 0) { return console.log(`Неверный isfix_rr, указан: ${isfix}`) };
 
@@ -74,27 +76,33 @@ let Delta_selector = {
         return [Math.floor(dW), Math.floor(dH)]
     },
 
+    door(isfix) {
+        if (isfix > 1 || isfix < 0) { return console.log(`Неверный isfix_door, указан: ${isfix}`) };
+
+        dH = (isfix) ? SizeDB.d_rr(this.sys()) : SizeDB.d_rs(this.sys());
+        dW = (isfix) ? SizeDB.d_rr(this.sys()) : SizeDB.d_rs(this.sys());
+        return [Math.floor(dW), Math.floor(dH)]
+    }
+
 }
 
 
 //!TODO: заработало, доделать!!
+
+
 function gonow() {
     let wintype = document.getElementById('fon').getAttribute('wintype');
-    const glasses = new MainSelector()[wintype]();
-    for (const glass of glasses) {
-        console.log(glass);
-    }
-
-
-
-
-
+    let glasses = new MainSelector()[wintype]();
+    let zh = applyZs(glasses);
+    console.log(`glasses: ${glasses}`);
+    console.log(`zhaluzi: ${zh}`);
+    return { zh };
 }
 
+//!----------<<<-----------  getSizes()    ------------->>>-------------//
 function getSizes() {
-    //!----------<<<-----------[Array.H, Array.W, objects]------------->>>-------------//
 
-    document.getElementById('mid').value = document.getElementById('w').value - document.getElementById('levo').value - document.getElementById('pravo').value;
+    // document.getElementById('mid').value = document.getElementById('w').value - document.getElementById('levo').value - document.getElementById('pravo').value;
     let wt = document.getElementById('fon').getAttribute("wintype");
     let sizes = document.getElementsByClassName("size");
     let hp = new Map();
@@ -107,6 +115,7 @@ function getSizes() {
         Array.from(wp.values()),
         Array.from(hp.values())
     ];
+    console.log({ sizepool });
     return sizepool
 }
 
@@ -135,15 +144,24 @@ class MainSelector {
         ]
     }
     fff(sizepool = getSizes()) {
-        let fix = IdSelector;
-        let g_left, g_mid, g_right;
+        let g_left, g_mid, g_right, g_w;
         g_left = new GLS(sizepool[0][0], sizepool[1][0]);
         g_right = new GLS(sizepool[0][1], sizepool[1][0]);
-        g_mid = new GLS(sizepool[0][2], sizepool[1][0]);
+        g_w = new GLS(sizepool[0][2], sizepool[1][0]);
+        g_mid = new GLS(g_w.w - g_left.w - g_right.w, g_w.h)
         return [
             g_left.applyDelta(Delta_selector.ri(this.check("s1"))),
             g_mid.applyDelta(Delta_selector.ii(this.check("s2"))),
             g_right.applyDelta(Delta_selector.ri(this.check("s3")))
+        ]
+    }
+    df(sizepool = getSizes()) {
+        let g_door, g_right;
+        g_door = new GLS(sizepool[0][0], sizepool[1][0]);
+        g_right = new GLS(sizepool[0][1] - sizepool[0][0], sizepool[1][0]);
+        return [
+            g_door.applyDelta(Delta_selector.ri(this.check("sd"))),
+            g_right.applyDelta(Delta_selector.rr(this.check("s2")))
         ]
     }
 }
@@ -152,7 +170,7 @@ const IdSelector = {
     idw: {
         f: ["w"],
         ff: ["levo", "w"],
-        fff: ["levo", "mid", "pravo"],
+        fff: ["levo", "w", "pravo"],
         df: ["w", "levo"],
         dff: ["w", "levo", "pravo"],
         fdf: ["levo", "w", "pravo"],
@@ -161,9 +179,9 @@ const IdSelector = {
         f: ["h"],
         ff: ["h", "h"],
         fff: ["h", "h", "h"],
-        df: ["h", "hpr"],
-        dff: ["h", "hpr", "hpr"],
-        fdf: ["hlv", "h", "hpr"],
+        df: ["h", "hpr", "himp"],
+        dff: ["h", "hpr", "hpr", "himp"],
+        fdf: ["hlv", "h", "hpr", "himp"],
     }
 
 }
